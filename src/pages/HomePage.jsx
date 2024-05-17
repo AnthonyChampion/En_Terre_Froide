5b11c39becfdebb8cc1a66462251e33e78911ff2
ANTHONY CHAMPION <anthonyrodrigues.c@gmail.com>
2024-05-17 12:53:11 +0200

    Style bg image

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,12 @@ import "./HomePage.css";
 export default function HomePage() {
     return (
         <section className="HomePageComponent">
-            <h1 className="home_title">Bienvenue en &quot;Terre Froide&quot;, créateur d&apos;expériences</h1>
-            <p className="concept_description">Embarquez pour des aventures uniques au coeur des terres glacées : explorez les merveilles des contrées scandinaves et polaires avec notre agence de voyages atypiques.</p>
-            <SliderTrips />
+            <div className="banner_container">
+                <h1 className="home_title">Bienvenue en &quot;Terre Froide&quot;, créateur d&apos;expériences</h1>
+                <p className="concept_description">Embarquez pour des aventures uniques au coeur des terres glacées : explorez les merveilles des contrées scandinaves et polaires avec notre agence de voyages atypiques.</p>
+                <SliderTrips />
+            </div>
+
             <LastTrip />
 
         </section>
